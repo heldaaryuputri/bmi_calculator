3be7b490a732a41b5e230348109b131c98137658
heldaaryuputri <162781938+heldaaryuputri@users.noreply.github.com>
2024-05-28 22:09:33 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 122, 59, 47)),
+        useMaterial3: true,
+      ),
+      home: const InputPage(title: 'BMI Calculator'),
+    );
+  }
+}
